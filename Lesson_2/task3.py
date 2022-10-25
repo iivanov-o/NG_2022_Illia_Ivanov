ce2b960ceae6348d1496a040bbb21b80f244b9ba
Illia559 <123123ilja@gmail.com>
2022-10-25 22:58:03 +0200

    added task3

---
+++ Lesson_2/task3.py
@@ -0,0 +1,18 @@
+from ast import Return
+
+
+userNumber = int(input(("Enter your number: ")))
+
+
+while userNumber > 0:
+    secondNumber = userNumber
+
+    while secondNumber > 0:
+        print (secondNumber, end='')
+        secondNumber = secondNumber - 1
+    
+    userNumber = userNumber - 1
+    print (" ")
+    
+
+
